38bc6a4c4516f8327de799d49b2590863e514aed
Kamran Azeem <kamranazeem@gmail.com>
2023-11-20 12:19:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Patients
+
+Simple web application to be used as an example for CI/CD demos. Listens on port 8080. To change the port number, or the message that is displayed on the web page, edit the `src/main.go` file.
+
+
---
+++ src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+)
+
+func main() {
+    http.HandleFunc("/", MainServer)
+
+    server_port := ":8080"
+
+    http.ListenAndServe(server_port, nil)
+}
+
+func MainServer(w http.ResponseWriter, r *http.Request) {
+    // keep changing the name of city to mimic change in the code.
+    fmt.Fprintf(w, os.Getenv("GREETING") + " Patients! We are located in: Oslo")
+}
