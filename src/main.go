2869441819d8052310d832ed414317c78418f9b0
Kamran Azeem <kamranazeem@gmail.com>
2024-06-18 00:14:27 +0200

    Introducts log package to write proper log entries/messages. Refactored application function names.

--- src/main.go
+++ src/main.go
@@ -4,23 +4,24 @@ import (
     "fmt"
     "net/http"
     "os"
+    "log"
 )
 
 func main() {
-    http.HandleFunc("/", MainServer)
+    http.HandleFunc("/", DisplayMainMessage)
 
     server_port := ":4444"
 
     // The following lines are used to log to console, not on the web UI
-    fmt.Println("Secret(s) and config files(s) were loaded.")
+    log.Println("Secret(s) and config files(s) were loaded.")
 
-    fmt.Printf("Starting application at port: %s \n", server_port)
+    log.Printf("Starting application at port: %s \n", server_port)
 
     http.ListenAndServe(server_port, nil)
 
 }
 
-func MainServer(w http.ResponseWriter, r *http.Request) {
+func DisplayMainMessage(w http.ResponseWriter, r *http.Request) {
     // Following is the main application functionality,
     //   which displays something on the web UI.
     // Keep changing the name of city to mimic change in the code.
