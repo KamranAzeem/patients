716a3611726e2513bdd169f9f6be248a4fc10e48
Kamran Azeem <kamranazeem@gmail.com>
2024-06-17 12:44:12 +0200

    Changed application port from 8080 to 4444. Added messages to be logged on application console for looging.

--- src/main.go
+++ src/main.go
@@ -9,12 +9,21 @@ import (
 func main() {
     http.HandleFunc("/", MainServer)
 
-    server_port := ":8080"
+    server_port := ":4444"
 
     http.ListenAndServe(server_port, nil)
 }
 
 func MainServer(w http.ResponseWriter, r *http.Request) {
-    // keep changing the name of city to mimic change in the code.
+    // The following lines are used to log to console, not on the web UI
+    fmt.Println("Secret(s) and config files(s) were loaded.")
+    server_port:=":4444"
+    fmt.Printf("Starting application at port: %s", server_port)
+    // Following is the main application functionality,
+    //   which displays something on the web UI.
+    // Keep changing the name of city to mimic change in the code.
     fmt.Fprintf(w, os.Getenv("GREETING") + " Patients! We are located in: Bodo")
+
 }
+
+
