50c8cb062d000ad47c4b85c029a67566454e4b9b
Kamran Azeem <kamranazeem@gmail.com>
2024-06-17 13:36:55 +0200

    Relocated log messages to main function. Added newlines to messages.

--- src/main.go
+++ src/main.go
@@ -11,18 +11,20 @@ func main() {
 
     server_port := ":4444"
 
+    // The following lines are used to log to console, not on the web UI
+    fmt.Println("Secret(s) and config files(s) were loaded.")
+
+    fmt.Printf("Starting application at port: %s \n", server_port)
+
     http.ListenAndServe(server_port, nil)
+
 }
 
 func MainServer(w http.ResponseWriter, r *http.Request) {
-    // The following lines are used to log to console, not on the web UI
-    fmt.Println("Secret(s) and config files(s) were loaded.")
-    server_port:=":4444"
-    fmt.Printf("Starting application at port: %s", server_port)
     // Following is the main application functionality,
     //   which displays something on the web UI.
     // Keep changing the name of city to mimic change in the code.
-    fmt.Fprintf(w, os.Getenv("GREETING") + " Patients! We are located in: Bodo")
+    fmt.Fprintf(w, os.Getenv("GREETING") + " Patients! We are located in: Bodo" + "\n")
 
 }
 
